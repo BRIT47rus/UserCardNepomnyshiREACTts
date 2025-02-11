2b492bc1f5848268b35e411740ea10802b6e152d
Vladimir Noutbook <vovirus2@inbox.ru>
2025-02-11 16:23:31 +0300

    мок данные

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,19 +1,19 @@
 import { LocalGithubUser } from "../components/type" 
 
 export const userMock: LocalGithubUser = {
-    "login": "brit",
+    "login": "Brit",
 
-    company: "EPAM",
+    company: "RTITS",
 
-    "name": "Mikhail Nepomniashchii",
-    "blog": "mishanep.com",
-    "location": "Malaga",
+    "name": "Владимир Федоров",
+    "blog": "vovirus",
+    "location": "Vyborg",
     "bio": "React developer",
-    avatar: "https://avatars.githubusercontent.com/u/36237995?v=4",
-    "repos": 82,
-    "followers": 690,
-    "following": 8,
+    avatar: "https://sun9-19.userapi.com/impg/uX7wQndU4Tdtv7-qeEz0xLyvP-S5IR3yq_LDsg/ESlhSKbUPnE.jpg?size=607x1080&quality=96&sign=0d650120e6b30a94858389aa124c957d&type=album",
+    "repos": 7,
+    "followers": 0,
+    "following": 0,
     "created": "2018-02-07T16:18:45Z",
-    twitter:"pcgramota"
+    twitter:"vovirus"
 
 }
